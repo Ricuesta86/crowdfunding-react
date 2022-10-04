7ad226efa79576b3c346df3931cea44452d50260
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-10-04 12:06:19 -0500

    Add style menu media query 440

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,66 @@
-import React, { useState } from 'react'
-import './Header.scss'
-import Menu from './Menu';
+import React, { useState } from "react";
+import "./Header.scss";
+import Menu from "./Menu";
 const Header = () => {
-  const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(true);
 
-
-  const handleTogget = ()=>{
-
-  }
+  const handleTogget = () => {};
 
   return (
-    <header className='header' >
-      <nav className='header__navbar'>
-        <div className='header__navbar__logo'><a href="#"><img className='header__navbar__logo-img' src="./images/logo.svg" alt="" /></a></div>
-        <div className='header__navbar__menu'>
-          <ul className='header__navbar__menu-ul'>
-            <li className='header__navbar__menu-li'><a href="#">About</a></li>
-            <li className='header__navbar__menu-li'><a href="#">Discover</a></li>
-            <li className='header__navbar__menu-li'><a href="#">Get Started</a></li>
+    <header className="header">
+      <nav className={show ? "header__navbar" : "header__navbar close"}>
+        <div className="header__navbar__logo">
+          <a href="#">
+            <img
+              className="header__navbar__logo-img"
+              src="./images/logo.svg"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="header__navbar__menu">
+          <ul className="header__navbar__menu-ul">
+            <li className="header__navbar__menu-li">
+              <a href="#">About</a>
+            </li>
+            <li className="header__navbar__menu-li">
+              <a href="#">Discover</a>
+            </li>
+            <li className="header__navbar__menu-li">
+              <a href="#">Get Started</a>
+            </li>
           </ul>
-          <img 
-          className={!show?'header__navbar__menu-hamburger ': ' header__navbar__menu-hamburger hide'}src="./images/icon-hamburger.svg" alt="hamburger" 
-          onClick={()=>setShow(!show)}
+          <img
+            className="header__navbar__menu-hamburger"
+            src={
+              show
+                ? "./images/icon-hamburger.svg"
+                : "./images/icon-close-menu.svg"
+            }
+            alt="Icon Menu"
+            onClick={() => setShow(!show)}
           />
-          {
-           show?<Menu isShow={show}/>:null
-          }
+          {!show ? (
+            <div className="header__navbar__menu__modal">
+              <div className="header__navbar__menu__modal--modal">
+                <ul className="header__navbar__menu__modal--ul">
+                  <li className="-header__navbar__menu__modal-li">
+                    <a href="#">About</a>
+                  </li>
+                  <li className="header__navbar__menu__modal--li">
+                    <a href="#">Discover</a>
+                  </li>
+                  <li className="header__navbar__menu__modal--li">
+                    <a href="#">Get Started</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ) : null}
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -7,14 +7,7 @@ const Menu = ({isShow}:prop) => {
     const [show, setShow] = useState<boolean>(isShow);
     return (
         <div>
-            <ul className='header__navbar__menu-ul1'>
-                <li className='header__navbar__menu-li'><a href="#">About</a></li>
-                <li className='header__navbar__menu-li'><a href="#">Discover</a></li>
-                <li className='header__navbar__menu-li'><a href="#">Get Started</a></li>
-            </ul>
-            <img src="./images/icon-close-menu.svg" alt="Close"
-            onClick={()=>setShow(!show)}
-            />            
+         
         </div>
     )
 }
