8be949dc2e11643741a906fd31763518dce73a1b
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-10-17 05:35:02 -0400

    Add style Backer

--- src/components/Backer/Backer.tsx
+++ src/components/Backer/Backer.tsx
@@ -1,28 +1,30 @@
 import { useAppSelector } from '../../reducers/hook';
+import { NumericFormat } from 'react-number-format';
 import { dataType } from '../../type';
 import './Backer.scss';
 import ProgressBar from './ProgressBar';
 
 
 const Backer = () => {
-    const data = useAppSelector(state=>state.mastercraft);
+    const data = useAppSelector(state => state.mastercraft);
     return (
         <>
             <section className="backer">
-                <div className='backed__group'>
-                    <div>
-                        <h2>${data.backed}</h2>
-                        <p>of ${data.totalBacked} backed</p>
-                        <div className="line"></div>
+                <div className='backer__group'>
+                    <div className='backer__group__backed'>
+                        <h2 className='backer__group__backed__title'>
+                            <NumericFormat className='backer__group__backed__title' value={data.backed} displayType={'text'} thousandSeparator={true} prefix="$" /></h2>
+                        <p className='backer__group__backed__text'>of <NumericFormat value={data.totalBacked} displayType={'text'} thousandSeparator={true} prefix="$" /> backed</p>
                     </div>
-                    <div>
-                        <h2>{data.totalBackers}</h2>
-                        <p>total backers</p>
-                        <div className="line"></div>
+                    <div className="backer__group__line"></div>
+                    <div className='backer__group__backed'>
+                        <h2 className='backer__group__backed__title'>{data.totalBackers}</h2>
+                        <p className='backer__group__backed__text'>total backers</p>
                     </div>
-                    <div>
-                        <h2>{data.dayLeft}</h2>
-                        <p>days left</p>
+                    <div className="backer__group__line"></div>
+                    <div className='backer__group__backed'>
+                        <h2 className='backer__group__backed__title'>{data.dayLeft}</h2>
+                        <p className='backer__group__backed__text'>days left</p>
                     </div>
                 </div>
                 <ProgressBar inicial={data.backed} total={data.totalBacked} />
