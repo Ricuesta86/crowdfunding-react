668e5f4591457e3aca527dcfbe3f049e659123b3
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-10-18 11:01:10 -0500

    Add style BackThisProject

--- src/api.ts
+++ src/api.ts
@@ -11,19 +11,19 @@ const api = {
       {
         title: "Bamboo Stand",
         pledge: 25,
-        text: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and youâ€™ll be added to a special Backer member list.",
+        text: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
         amount: 101,
       },
       {
         title: "Black Edition Stand",
         pledge:75,
-        text: "You get a Black Special Edition computer stand and a personal thank you. Youâ€™ll be added to our Backer member list. Shipping is included.",
+        text: "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
         amount: 64,
       },
       {
-        title: "Black Edition Stand",
+        title: "Mahogany Special Edition",
         pledge: 200,
-        text: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a        personal thank you. Youâ€™ll be added to our Backer member list. Shipping is included.",
+        text: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a        personal thank you. You'll be added to our Backer member list. Shipping is included.",
         amount: 0,
       },
     ],
--- src/components/BackThisProject/BackThisProject.tsx
+++ src/components/BackThisProject/BackThisProject.tsx
@@ -1,95 +1,102 @@
-import { useState } from 'react'
-import { NumericFormat } from 'react-number-format';
-import api from '../../api';
-import { product } from '../../type';
-import Modal from '../Modal/Modal'
-import Thanks from '../Thanks/Thanks';
-import './BackThisProject.scss'
-import { useAppSelector,useAppDispatch } from '../../reducers/hook';
-import { backedAdded, backersIncremented } from '../../reducers/mastercraftReducer';
-import { pledgeDecremented } from '../../reducers/pledgeReducer';
+import { useState } from "react";
+import { NumericFormat } from "react-number-format";
+import api from "../../api";
+import { product } from "../../type";
+import Modal from "../Modal/Modal";
+import Thanks from "../Thanks/Thanks";
+import "./BackThisProject.scss";
+import { useAppSelector, useAppDispatch } from "../../reducers/hook";
+import {
+  backedAdded,
+  backersIncremented,
+} from "../../reducers/mastercraftReducer";
+import { pledgeDecremented } from "../../reducers/pledgeReducer";
 
 type props = {
-    handleClose: any;
-    pledgeProp: number | undefined;
-
-}
+  handleClose: any;
+  pledgeProp: number | undefined;
+};
 const BackThisProject = ({ handleClose, pledgeProp }: props) => {
-    const products = useAppSelector(state=>state.pledge);
-    const dispatch = useAppDispatch();
-    const [pledge, setPledge] = useState<number | undefined>(pledgeProp);
-    const [text, setText] = useState<number>(pledgeProp ? pledgeProp : 0);
-    const [showThanks, setShowThanks] = useState(false);
+  const products = useAppSelector((state) => state.pledge);
+  const dispatch = useAppDispatch();
+  const [pledge, setPledge] = useState<number | undefined>(pledgeProp);
+  const [text, setText] = useState<number>(pledgeProp ? pledgeProp : 0);
+  const [showThanks, setShowThanks] = useState(false);
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        if (event.target.pledge.value >= event.target.price.value) {
-            let backed = Number(event.target.pledge.value);
-            dispatch(backedAdded({backed}));
-            dispatch(backersIncremented({}));
-            dispatch(pledgeDecremented({pledge}));
-            setShowThanks(true);
-        }
-
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (event.target.pledge.value >= event.target.price.value) {
+      let backed = Number(event.target.pledge.value);
+      dispatch(backedAdded({ backed }));
+      dispatch(backersIncremented({}));
+      dispatch(pledgeDecremented({ pledge }));
+      setShowThanks(true);
     }
+  };
 
-    const handleSelect = (value: number) => {
-        setPledge(value);
-        setText(value);
-    }
+  const handleSelect = (value: number) => {
+    setPledge(value);
+    setText(value);
+  };
 
-    const handleChange = (event: any) => {
-        setText(event.target.value);
-    }
-    return (
-        <>
-            <Modal handleClose={handleClose}>
-                <div>
-                    <h3>Back this project {pledge}</h3>
-                    <p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
-                    <section>
-                        <div>
-                            Pledge with no reward
-                        </div>
-                        <p>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.
-                        </p>
-                    </section>
-                </div>
+  const handleChange = (event: any) => {
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <Modal handleClose={handleClose}>
+        <div className="thisproject">
+          <h3 className="thisproject__title">Back this project</h3>
+          <p className="text">
+            Want to support us in bringing Mastercraft Bamboo Monitor Riser out
+            in the world?
+          </p>
+          <div className="thisproject__pledge">
+            <div>Pledge with no reward</div>
+            <p className="text">
+              Choose to support us without a reward if you simply believe in our
+              project. As a backer, you will be signed up to receive product
+              updates via email.
+            </p>
+          </div>
+        </div>
 
-                {/* <!-- Selection modal start --> */}
-                <section>
-                    {
-                        products.map((product, index) => (
-                            <div key={index} className=''>
-                                <div onClick={() => handleSelect(product.pledge)}>
-                                    <h3>{product.title}</h3>
-                                    <p>"Pledge ${product.pledge} or more"</p>
-                                </div>
-                                <p>{product.text}
-                                </p>
+        {/* <!-- Selection modal start --> */}
+        <section>
+          {products.map((product, index) => (
+            <div key={index} className="">
+              <div onClick={() => handleSelect(product.pledge)}>
+                <h3>{product.title}</h3>
+                <p>"Pledge ${product.pledge} or more"</p>
+              </div>
+              <p>{product.text}</p>
 
-                                <div>
-                                    <h4>
-                                        {product.amount} <span className="h4--span"> left</span>
-                                    </h4>
-                                </div>
-                                <div className='line'></div>
-                                <div style={product.pledge !== pledge ? { display: 'none' } : {}}>
-                                    <form onSubmit={(event) => handleSubmit(event)}>
-                                        <p>Enter your pledge</p>
-                                        <NumericFormat value={text} name='pledge' onChange={event => handleChange(event)} thousandSeparator={true}/>
-                                        <input type={'hidden'} value={product.pledge} name='price' />
-                                        <button>Continue</button>
-                                    </form>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </section>
-            </Modal>
-            {showThanks ? <Thanks /> : ''}
-        </>
-    )
-}
+              <div>
+                <h4>
+                  {product.amount} <span className="h4--span"> left</span>
+                </h4>
+              </div>
+              <div className="line"></div>
+              <div style={product.pledge !== pledge ? { display: "none" } : {}}>
+                <form onSubmit={(event) => handleSubmit(event)}>
+                  <p>Enter your pledge</p>
+                  <NumericFormat
+                    value={text}
+                    name="pledge"
+                    onChange={(event) => handleChange(event)}
+                    thousandSeparator={true}
+                  />
+                  <input type={"hidden"} value={product.pledge} name="price" />
+                  <button>Continue</button>
+                </form>
+              </div>
+            </div>
+          ))}
+        </section>
+      </Modal>
+      {showThanks ? <Thanks /> : ""}
+    </>
+  );
+};
 
-export default BackThisProject
+export default BackThisProject;
