3f8381da4eed0451663f0db71be6bd20cf39f5a6
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-10-20 08:23:59 -0400

    Add style Thanks

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -6,12 +6,12 @@ type Props={
 }
 const Thanks = ({handleClose}:Props) => {
   return (
-    <div>
-      <img src="./images/icon-check.svg" alt="Check" />
-      <h1>Thanks for your support!</h1>
-      <p>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
+    <div className='thanks'>
+      <img className='thanks__img' src="./images/icon-check.svg" alt="Check" />
+      <h1 className='thanks__title' >Thanks for your support!</h1>
+      <p className='text'>Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get
       an email once our campaign is completed.</p>
-      <button onClick={handleClose}>Got it!</button>
+      <button className='btn' onClick={handleClose}>Got it!</button>
           
           
           
