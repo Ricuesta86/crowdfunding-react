79e8005deab265e5de5c06003563031b002e0f34
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-10-22 06:11:44 -0400

    Finished style Thanks

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -8,7 +8,7 @@ type props = {
 }
 const Modal = ({ close, children, handleClose }: props) => {
   return (
-    <div className='modal' style={{display:'block'}}>
+    <div className='modal' style={{display:'block',paddingLeft:'0px'}}>
       <div className={!close? 'modal__container-width':'modal__container'}>
         {close && <img onClick={() => handleClose()} className='modal__container__close' src="./images/icon-close-modal.svg" alt="Close modal" />}
         {children}
